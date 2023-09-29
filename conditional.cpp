df8356b2a894610a0f2e1e54eb38c5a16f6e57ca
bhanupjha <mailtobhanup@gmail.com>
2023-09-29 19:17:42 +0530

    new program added

---
+++ conditional.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//***********if statement****************
+    // int a;       // Here if we will give -ve no.or 0, if block doesnot exceute 
+    // cin >> a;    // and the program exits without printing anything
+    // if(a > 0){
+    //     cout << "a is positive" << endl;  
+    // }
+
+//compare two number using if block
+    // int a, b;
+    // cout << "Enter the value of a: " << endl;
+    // cin >> a;
+    // cout << "Enter the value of b: " << endl;
+    // cin >> b;
+    // if(a >  b){
+    //     cout << "a is greater than b"<<endl;
+    // }
+    // if(b > a){
+    //     cout<<"b is greater than a"<<endl;
+    // }
+
+//***********if else statement*****************
+    // int a;
+    // cin>>a;
+    // if(a > 0){
+    //     cout<< "a is positive" << endl;
+    // }
+    // else{                                   // In this else block, either a will be negative or 0.
+    //     cout<< "a is not positive" << endl;   
+    // }
+
+//**************Nested if*********************
+    // int a;
+    // cin>>a;
+    // if(a > 0){
+    //     cout<< "a is positive" << endl;
+    // }
+    // else{  
+    //     if(a == 0){
+    //         cout<<"a is 0" << endl;
+    //     } 
+    //     else{                              
+    //     cout<< "a is negative" << endl;   
+    //     }
+    // }
+
+//*************else if*************************
+    // int a;
+    // cout<<"Enter the value of a: " << endl; 
+    // cin>>a;
+    // if(a > 0){
+    //     cout<< "a is positive" << endl;
+    // }
+    // else if(a < 0){                              
+    //     cout<< "a is negative" << endl;   
+    //     }
+    // else{
+    //     cout<< "a is 0" << endl;
+    // }    
+
+//*************practice*************
+//Example 1:
+    // int a = 9;
+    // if(a == 9){
+    //     cout<<"NINEY";
+    // }
+    // if(a > 0){
+    //     cout<<"POSITIVE";
+    // }
+    // else{
+    //     cout<<"NEGATIVE";
+    // }
+
+//Example 2:
+    // int a = 2;
+    // int b = a + 1;
+    // if((a=3)==b){
+    //     cout<<a;
+    // }
+    // else{
+    //     cout<<a+1;
+    // }
+
+//Example 3:
+        // int a = 24;
+        // if(a > 20){
+        //     cout<<"LOVE";
+        // }
+        // else if(a == 24){
+        //     cout<<"LOVELY";
+        // }
+        // else{
+        //     cout<<"hello";
+        // }
+        // cout<<a;
+
+//Example 4:
+        //  char ch;
+        //  cin>>ch;
+        //  // 'A' is 65
+        //  // 'a' is 97
+        //  // '0' is 48
+        //  //  we can also use ASCII value
+        //  if(ch >= 'A' && ch <= 'Z'){
+        //     cout<<"This is upper case"<<endl;
+        //  }
+        //  else if(ch >= 'a' && ch <= 'z'){
+        //     cout<<"This is in lower case"<<endl;
+        //  }
+        //  else if(ch >= '0' && ch <= '9'){
+        //     cout<<"This is in Numeric form"<<endl; 
+        //  }
+
+//Example 5:
+    // int age;
+    // cout<<"Enter the age: "<<endl;
+    // cin>>age;
+    // if(age>50){
+    //     cout<<"you are old"<<endl;
+    // }
+    // else if(age<20){
+    //     cout<<"You are child or student"<<endl;
+    // }
+    // else{
+    //     cout<<"you are young"<<endl;
+    // }
+
+     return 0;
+}
---
+++ inputAndOutput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Value of a just after intialization: "<<a<<endl;
+    cin>>a;
+    cout<<"Value of a just after cin statement: "<<a<<endl;
+    return 0;
+}
